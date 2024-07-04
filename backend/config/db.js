78592a8303793b2f4a4733747bf0edc94e2b32c7
kesharibhai84 <vishalkeshari2004@gmail.com>
2024-07-04 19:02:19 +0530

    h

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect();
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
