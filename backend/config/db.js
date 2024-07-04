b6b5e8f2c76c73854217fe7207c453963dab4472
kesharibhai84 <vishalkeshari2004@gmail.com>
2024-07-04 20:01:46 +0530

    j

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,8 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
